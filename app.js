87c276ce83f036b21649e1b14b058fa707edb54a
carlos <carlos811009@gmail.com>
2021-06-19 18:22:19 +0800

    Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -17,11 +18,13 @@ app.use((req, res, next) => {
   res.locals.error_messages = req.flash('error_messages')
   next()
 })
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
