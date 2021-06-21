d1ddac748c00bca0f8be37d239d64f6cd5b91547
carlos <carlos811009@gmail.com>
2021-06-21 21:59:38 +0800

    add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,24 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => {
+        const data = restaurants.map(each => {
+          return {
+            ...each,
+            description: each.description.substring(0, 50),
+            CategoryName: Category.name
+          }
+        })
+        return res.render('restaurants', { restaurants: data })
+      })
   }
 }
 module.exports = restController
--- seeders/20210619124444-users-seed-file.js
+++ seeders/20210619124444-users-seed-file.js
@@ -2,7 +2,7 @@
 const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
+    await queryInterface.bulkInsert('Users', {
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -23,7 +23,7 @@ module.exports = {
       name: 'user2',
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {})
+    }, {})
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Users', null, {})
