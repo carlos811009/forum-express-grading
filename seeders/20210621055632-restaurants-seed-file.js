1ac139fa9336328d4d2e53fe65cf6151c5db118a
carlos <carlos811009@gmail.com>
2021-06-21 14:06:44 +0800

    Add category seed data

--- seeders/20210621055632-restaurants-seed-file.js
+++ seeders/20210621055632-restaurants-seed-file.js
@@ -4,7 +4,7 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Restaurants',
-      Array.from({ length: 50 }).map((d, i) =>
+      Array.from({ length: 50 }).map(d =>
       ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
@@ -13,10 +13,10 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date(),
-        CategoryId: Math.floor(Math.random() * 6) * 10 + 1
+        updatedAt: new Date(), // 加逗點
+        CategoryId: Math.floor(Math.random() * 6) * 10 + 1 // 加上這行
       })
-      ), {})
+      ), {});
   },
 
   down: async (queryInterface, Sequelize) => {
