bf81c15792e7fbc691e05ef3fd57879930d397c7
carlos <carlos811009@gmail.com>
2021-06-21 13:24:05 +0800

    Add restaurant.CategoryId and association

--- migrations/20210621051632-add-image-to-restaurants.js
+++ migrations/20210621051632-add-image-to-restaurants.js
@@ -4,9 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'image', {
       type: Sequelize.STRING
-    })
+    });
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Restaurants', 'image');
   }
