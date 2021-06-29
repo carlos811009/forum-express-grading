9695d1e3e4cb5ba5a03bb9bdb20251ab906f22b0
carlos811009 <carlos811009@gmail.com>
2021-06-29 16:51:55 +0800

    後台修改個別餐廳

--- routes/apis.js
+++ routes/apis.js
@@ -10,5 +10,6 @@ router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', adminController.getCategories)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 module.exports = router
